5ec5b34f13967c23b06ded97d4ac39632e741012
Lucca Rendall <luccarendall1@gmail.com>
2022-04-07 18:43:01 -0300

    Grupo 20 - Requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <h1 data-testid="feedback-text">
           Feedback
         </h1>
